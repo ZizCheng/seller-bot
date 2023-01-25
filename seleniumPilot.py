ba7b60df7f3b3d74601547e78ec5aa538b598c95
pigfyy <111782362+pigfyy@users.noreply.github.com>
2023-01-24 21:41:47 -0800

    s34

--- seleniumPilot.py
+++ seleniumPilot.py
@@ -1,17 +1,13 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+import env
 import undetected_chromedriver as webdriver
+import time
 
-# Create a new instance of the Chrome driver with the --remote-debugging-port option
-chrome_options = Options()
-chrome_options.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
-driver = webdriver.Chrome(options=chrome_options)
 
-# Navigate to the webpage
-driver.get("https://www.google.com/")
 
-# Find the textbox element by its id
-textbox = driver.find_element_by_id("input.gLFyf")
+options = webdriver.ChromeOptions()
+profile = dir
+options.add_argument(f"user-data-dir={profile}")
+driver = webdriver.Chrome(options=options, use_subprocess=True)
+driver.get("https://amazon.com")
 
-# Input text into the textbox
-textbox.send_keys("Hello World!")
+time.sleep(1000000)
